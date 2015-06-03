58bf42864e7b26d85ccf7ecd001160439f720457
Bob den Otter <bobdenotter@gmail.com>
2015-06-03 17:13:00 +0200

    Making extension work on 2.3

--- Extension.php
+++ Extension.php
@@ -4,34 +4,41 @@ namespace Bolt\Extension\BobdenOtter\GridField;
 
 use Bolt\Application;
 use Bolt\BaseExtension;
+use Symfony\Component\HttpFoundation\Request;
+use Bolt\Controller\Zone;
 
 require_once('GridField.php');
 
 class Extension extends BaseExtension
 {
-  
+
 
     public function __construct(Application $app)
     {
         parent::__construct($app);
         $this->app['config']->getFields()->addField(new GridField());
-        if ($this->app['config']->getWhichEnd()=='backend') {
-            $this->app['htmlsnippets'] = true;
-            $this->app['twig.loader.filesystem']->prependPath(__DIR__."/twig");
-        }
+
     }
 
     public function initialize() {
-        if ($this->app['config']->getWhichEnd()=='backend') {
-            $this->addCss('assets/handsontable.full.min.css');
-            $this->addJavascript('assets/handsontable.full.min.js', true);
-            $this->addJavascript('assets/start.js', true);
-        }
+        $this->app->before(array($this, 'addAssets'));
     }
 
     public function getName()
     {
-        return "gridfield";
+        return "Gridfield";
+    }
+
+    public function addAssets(Request $request)
+    {
+        if (Zone::isBackend($request)) {
+            $request->attributes->set('allow_snippets', true);
+            $this->addCss('assets/handsontable.full.min.css');
+            $this->addJavascript('assets/handsontable.full.min.js', true);
+            $this->addJavascript('assets/start.js', true);
+        }
+        $this->app['twig.loader.filesystem']->prependPath(__DIR__."/twig");
+
     }
 
 }
